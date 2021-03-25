dbbad9ea7d7f2be130c9f62f823e7a965dcf64eb
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-25 19:06:11 +0100

    add home, hours and projects page

--- timetracker/views.py
+++ timetracker/views.py
@@ -5,4 +5,14 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
+
+
+@views.route('/hours')
+def hours():
+    return render_template('hours.html')
+
+
+@views.route('/projects')
+def projects():
+    return render_template('projects.html')
