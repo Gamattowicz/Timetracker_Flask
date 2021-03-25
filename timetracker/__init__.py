6049dc1f773df6df02f7cd2524c04d69dd5ea4ba
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-25 17:51:41 +0100

    add blueprint views

--- timetracker/__init__.py
+++ timetracker/__init__.py
@@ -6,4 +6,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
     return app
