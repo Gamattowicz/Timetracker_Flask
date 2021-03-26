0c092cff22813965bb60251f957918fc7b9cba37
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-26 20:23:43 +0100

    start working with projects form

--- timetracker/views.py
+++ timetracker/views.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        print('<h1> Good </h1>')
     return render_template('home.html')
 
 
@@ -13,6 +15,8 @@ def hours():
     return render_template('hours.html')
 
 
-@views.route('/projects')
+@views.route('/projects', methods=['GET', 'POST'])
 def projects():
+    if request.method == 'POST':
+        return f'<h1> Name: {request.form["project"]} </h1>'
     return render_template('projects.html')
