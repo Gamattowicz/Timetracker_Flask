866aacb2d23f5725c1e3f87b8f45974e3c7fd6a2
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-29 19:22:52 +0200

    connecting to the database by SQLAlchemy

--- config.py
+++ config.py
@@ -5,7 +5,10 @@ from pathlib import Path
 base_dir = Path(__file__).resolve().parent
 env_file = base_dir / '.env'
 load_dotenv(env_file)
+DB_NAME = 'timetracker.db'
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{DB_NAME}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
