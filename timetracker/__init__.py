c2b95f2124d9876d212af7f379a490f8cca98f14
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-30 21:04:39 +0200

    add User schema model and user loader

--- timetracker/__init__.py
+++ timetracker/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config, DB_NAME
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
@@ -16,9 +17,17 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
 
-    from .models import Projects, Hours
+    from .models import Projects, Hours, User
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
 
     return app
 
--- timetracker/models.py
+++ timetracker/models.py
@@ -1,17 +1,24 @@
 from . import db
 from sqlalchemy.sql import func
+from flask_login import UserMixin
 
 
 class Projects(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=True, unique=True)
-    shortcut = db.Column(db.String(50), nullable=True, unique=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    shortcut = db.Column(db.String(50), nullable=False, unique=True)
     hour = db.relationship('Hours')
 
 
 class Hours(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Integer, nullable=True)
+    amount = db.Column(db.Integer, nullable=False)
     work_date = db.Column(db.DateTime(timezone=True), default=func.now())
     project_shortcut = db.Column(db.String(50), db.ForeignKey(
-        'projects.shortcut'), nullable=True)
+        'projects.shortcut'), nullable=False)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(50))
