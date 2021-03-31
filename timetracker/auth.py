1262261b73febf6e823d8185b73989a780b3f5b8
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-31 08:46:56 +0200

    add login function

--- timetracker/auth.py
+++ timetracker/auth.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, current_user
 
 
 auth = Blueprint('auth', __name__)
@@ -9,7 +10,22 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('You have logged in correctly', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password!', category='error')
+        else:
+            flash('User with that username does not exist!', category='error')
+
+    return render_template('login.html', user=current_user)
 
 
 @auth.route('sign-up', methods=['GET', 'POST'])
@@ -36,9 +52,11 @@ def sign_up():
                                                             method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('User has been created!', category='success')
             return redirect(url_for('views.home'))
-    return render_template('sign_up.html')
+
+    return render_template('sign_up.html', user=current_user)
 
 
 @auth.route('logout')
